2fe32414ebc59a442e2138f5673896d39d2458b2
Elizabeth-Bakul <bakelal@mail.ru>
2021-04-11 02:30:18 +0300

    Fourth hard-task

--- hard-task/script.js
+++ hard-task/script.js
@@ -1,56 +1,19 @@
 "use strict";
 
-let lang = prompt("Введите язык (ru/en): ");
+let string = prompt("Введите строку");
 
-let dayRus = [
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
-];
-
-let dayEng = [
-  "Monday",
-  "Tuesday",
-  "Wendensday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
-
-
-if (lang === 'ru') {
-  console.log(dayRus);
-} else if (lang === 'en') {
-  console.log(dayEng);
-} else {
-  console.log('Неправильный язык');
-}
-
-switch (lang) {
-  case "ru":
-    console.log(dayRus);
-    break;
-  case "en":
-    console.log(dayEng);
-    break;
-  default:
-    console.log('Неправильный язык');
-}
-
-let day = {
-  'ru':  dayRus,
-  'en': dayEng
+let lineUpgrade = function (string) {
+  if (typeof string === "string") {
+    string = string.trim();
+    if (string.length > 30) {
+      string = string.substr(0, 30) + "...";
+    }
+    return string;
+  } else {
+    return "Это не строка";
+  }
 };
-console.log(day[lang]);
 
-
-//task 2
-let namePerson = prompt("Имя участника курса: ");
-let profession;
-namePerson === 'Артём' ?  profession = 'Директор' :  namePerson === 'Максим' ? profession = 'Преподаватель' : profession = 'Студент';
-console.log(profession);
+console.log(lineUpgrade(string));
+console.log(lineUpgrade(5));
+console.log(lineUpgrade(true));
