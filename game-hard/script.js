25fc271f6402366a8de2383ff4402a2ebd03ecef
Elizabeth-Bakul <61085706+Elizabeth-Bakul@users.noreply.github.com>
2021-04-15 20:33:30 +0300

    Add files via upload

--- game-hard/script.js
+++ game-hard/script.js
@@ -12,7 +12,6 @@ function getRandomInt(min, max) {
 
 function game() {
   let hiddenNumber = getRandomInt(1, 100);
-  console.log(hiddenNumber);
   let numberOfAttempts = 10;
   function action() {
     if (numberOfAttempts < 1) {
@@ -24,14 +23,15 @@ function game() {
       }
     } else {
       let estimatedNumber = prompt("Угадай число от 1 до 100");
-      console.log(estimatedNumber);
       if (estimatedNumber === null) {
         alert("Игра окончена");
       }
       if (isNumber(estimatedNumber)) {
         numberOfAttempts--;
         if (estimatedNumber > hiddenNumber) {
-          alert("Загаданное число меньше. Осталось попыток " + numberOfAttempts);
+          alert(
+            "Загаданное число меньше. Осталось попыток " + numberOfAttempts
+          );
           action();
         } else if (+estimatedNumber === hiddenNumber) {
           if (confirm("Вы угадали! Хотели бы сыграть ещё?")) {
