8c79c7f475f14ec5a39985fd601776f36048bb91
Elizabeth-Bakul <bakelal@mail.ru>
2021-04-16 00:25:52 +0300

    Hard-task

--- hard-task/script.js
+++ hard-task/script.js
@@ -1,54 +1,21 @@
 "use strict";
-console.log("Task 1");
-let arr = ["3458", "4356", "345678", "1234", "26668", "4555890", "22234434"];
-
-console.log("Вариант 1");
-arr.forEach(function (item) {
-  if (item.charAt(0) === "2" || item.charAt(0) === "4") {
-    console.log(item);
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' , 'Воскресенье'];
+let date = new Date();
+let day = date.getDay();
+if (day === 0) {
+  day=6;
+} else { day--;}
+for (let i = 0, len = week.length; i < len; i++) {
+  let html = week[i];
+  if (i === day) {
+    html = html.bold();
   }
-});
-
-console.log("Вариант 2");
-arr.forEach(function (item) {
-  if (item.startsWith("2") || item.startsWith("4")) {
-    console.log(item);
+  if (i>4) {
+    html = html.italics();
   }
-});
-
-console.log("Task 2");
-console.log("Вариант 1");
-let n = 100;
-
-nextPrime: for (let i = 2; i <= n; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j === 0) {
-      continue nextPrime;
-    }
-  }
-  console.log("Простое число " + i + ".Делители числа 1 и " + i);
+  const div = document.createElement("div");
+  div.innerHTML = html;
+  document.body.appendChild(div);
 }
-console.log("Вариант 2");
-for (let i = 2; i <= n; i++) {
-  let j = 2;
-  let flag = false;
-  while (j < i && !flag) {
-    if (i % j === 0) {
-      flag = true;
-    }
-    j++;
-  }
-  if (!flag) {
-    console.log("Простое число " + i + ".Делители числа 1 и " + i);
-  }
-}
-console.log("Вариант 3 - Решето Эратосфена");
-let prime = Array(101).fill(true);
-for (var i = 2; i < 100; ++i) {
-  if (prime[i]) {
-    console.log("Простое число " + i + ".Делители числа 1 и " + i);
-    for (var j = i * i; j < 100; j += i) {
-      prime[j] = false;
-    }
-  }
-}
+
+
