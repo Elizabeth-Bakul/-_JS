8118461271c6bbcf0e98f076f6129949f1eb35cd
Elizabeth-Bakul <bakelal@mail.ru>
2021-04-16 17:23:24 +0300

    Hard-task update

--- hard-task/script.js
+++ hard-task/script.js
@@ -1,21 +1,32 @@
 "use strict";
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' , 'Воскресенье'];
+let days = [
+  "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
 let date = new Date();
 let day = date.getDay();
-if (day === 0) {
-  day=6;
-} else { day--;}
-for (let i = 0, len = week.length; i < len; i++) {
-  let html = week[i];
-  if (i === day) {
-    html = html.bold();
+let dayWeek = days[day];
+
+
+
+for (let item of week) {
+  let html = item;
+  if ((item === 'Суббота') || (item === 'Воскресенье')){
+  html = html.italics();
   }
-  if (i>4) {
-    html = html.italics();
+  if (item === dayWeek){
+    html = html.bold();
   }
   const div = document.createElement("div");
   div.innerHTML = html;
   document.body.appendChild(div);
+
 }
 
 
