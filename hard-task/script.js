3472020910b1118bf65484afedb38e089abb8c5a
Elizabeth-Bakul <bakelal@mail.ru>
2021-04-17 19:49:45 +0300

    hard-task

--- hard-task/script.js
+++ hard-task/script.js
@@ -1,5 +1,4 @@
 "use strict";
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' , 'Воскресенье'];
 let days = [
   "Воскресенье",
   "Понедельник",
@@ -9,24 +8,104 @@ let days = [
   "Пятница",
   "Суббота",
 ];
-let date = new Date();
-let day = date.getDay();
-let dayWeek = days[day];
+let months = [
+  "января",
+  "февраля",
+  "марта",
+  "апреля",
+  "мая",
+  "июня",
+  "июля",
+  "августа",
+  "сентября",
+  "октября",
+  "ноября",
+  "декабря",
+];
+const div = document.createElement("div");
+div.style.color="red";
+const div2 = document.createElement("div");
+div2.style.color = "red";
 
-
-
-for (let item of week) {
-  let html = item;
-  if ((item === 'Суббота') || (item === 'Воскресенье')){
-  html = html.italics();
+function bendTheWord(n, textForms) {
+  n = Math.abs(n) % 100;
+  var n1 = n % 10;
+  if (n > 10 && n < 20) {
+    return textForms[2];
   }
-  if (item === dayWeek){
-    html = html.bold();
+  if (n1 > 1 && n1 < 5) {
+    return textForms[1];
   }
-  const div = document.createElement("div");
-  div.innerHTML = html;
-  document.body.appendChild(div);
-
+  if (n1 === 1) {
+    return textForms[0];
+  }
+  return textForms[2];
 }
 
+function addZero(n){
+  if (n>=0 && n<10) {
+    return '0'+n;
+  } else {
+    return n;
+  }
+}
 
+function getData() {
+  let date = new Date();
+  let dayWeek = days[date.getDay()];
+  let day = date.getDate();
+  let numberMonth = date.getMonth() + 1;
+  let month = months[numberMonth - 1];
+  let year = date.getFullYear();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  let html1 =
+    "Сегодня" +
+    " " +
+    dayWeek +
+    ", " +
+    day +
+    " " +
+    month +
+    " " +
+    year +
+    " " +
+    "года" +
+    ", " +
+    hour +
+    " " +
+    bendTheWord(hour, ["час", "часа", "часов"]) +
+    " " +
+    minutes +
+    " " +
+    bendTheWord(minutes, ["минута", "минуты", "минут"]) +
+    " " +
+    seconds +
+    " " +
+    bendTheWord(seconds, ["секунда", "секунды", "секунд"]);
+  
+  div.innerHTML = html1;
+  
+  let html2 =
+    addZero(day) +
+    "." +
+    addZero(numberMonth) +
+    "." +
+    year +
+    " - " +
+    addZero(hour) +
+    ":" +
+    addZero(minutes) +
+    ":" +
+    addZero(seconds);
+  div2.innerHTML = html2;
+}
+
+document.body.appendChild(div);
+document.body.appendChild(div2);
+setInterval(()=> {
+  console.log('Привет');
+  getData();
+}, 1000);
