44e4b0f5e08f90e787f3c7b7b6d52fd7ae52521e
Elizabeth-Bakul <bakelal@mail.ru>
2021-04-24 17:36:44 +0300

    task

---
+++ part-1/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function DomElement(selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+}
+DomElement.prototype.newElement = function () {
+  let elem;
+  if (this.selector[0] === ".") {
+    elem = document.createElement("div");
+    elem.className = this.selector.slice(1);
+  }
+  if (this.selector[0] === "#") {
+    elem = document.createElement("p");
+    elem.id = this.selector.slice(1);
+  }
+  elem.style.cssText = `height: ${this.height}px;
+  width: ${this.width}px;
+  background: ${this.bg};
+  font-size: ${this.fontSize}px;
+  `;
+  console.log(elem);
+  return elem;
+};
+
+let newDiv = new DomElement(".block", 100, 100, "yellow", 14);
+let newP = new DomElement("#best", 15, 200, "blue", 16);
+
+document.body.appendChild(newDiv.newElement());
+document.body.appendChild(newP.newElement());
