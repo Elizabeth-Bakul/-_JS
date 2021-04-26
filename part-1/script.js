902bf32760c88115df56e6290e67359b2a23bf6f
Elizabeth-Bakul <bakelal@mail.ru>
2021-04-26 17:40:12 +0300

    part-2

--- part-1/script.js
+++ part-1/script.js
@@ -1,22 +1,35 @@
 "use strict";
 
-class First {
-  constructor() {}
-  hello() {
-    console.log("Привет я метод родителя!");
-  }
-}
+const myLesson = [
+  { lesson: 1, type: "basic", points: 2 },
+  { lesson: 2, type: "additional", points: 4 },
+  { lesson: 3, type: "basic", points: 6 },
+  { lesson: 4, type: "additional", points: 3 },
+  { lesson: 5, type: "basic", points: 4 },
+  { lesson: 6, type: "basic", points: 2 },
+  { lesson: 7, type: "additional", points: 2 },
+  { lesson: 8, type: "basic", points: 6 },
+  { lesson: 9, type: "basic", points: 4 },
+  { lesson: 10, type: "basic", points: 6 },
+  { lesson: 11, type: "additional", points: 5 },
+  { lesson: 12, type: "basic", points: 2 },
+  { lesson: 13, type: "additional", points: 2 },
+  { lesson: 14, type: "basic", points: 4 },
+  { lesson: 15, type: "additional", points: 1 },
+  { lesson: 16, type: "additional", points: 7 },
+];
 
-class Second extends First {
-  constructor() {
-    super();
+const someFunction = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].type === "basic") {
+      arr[i].points /= 2;
+    } else {
+      arr.splice(i, 1);
+      i--;
+    }
   }
-  hello() {
-    super.hello();
-    console.log("А я наследуемый метод!");
-  }
-}
+};
 
-const test = new Second();
+someFunction(myLesson);
 
-test.hello();
+console.log(myLesson);
