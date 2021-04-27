1d5a565a31fa204d7dcc5da89b8a604ce264f3f4
Elizabeth-Bakul <bakelal@mail.ru>
2021-04-27 17:48:26 +0300

    task

--- main-task/script.js
+++ main-task/script.js
@@ -39,5 +39,71 @@ window.addEventListener("DOMContentLoaded", () => {
     idInterval = setInterval(updateClock, 1000);
   }
 
-  countTimer("27 april 2020");
+  countTimer("28 april 2021");
+
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector(".menu"),
+      menu = document.querySelector("menu"),
+      closeBtn = document.querySelector(".close-btn"),
+      menuItems = menu.querySelectorAll("ul>li");
+
+    const handlerMenu = () => {
+      menu.classList.toggle("active-menu");
+    };
+
+    btnMenu.addEventListener("click", handlerMenu);
+    closeBtn.addEventListener("click", handlerMenu);
+    menuItems.forEach((item) => item.addEventListener("click", handlerMenu));
+  };
+  toggleMenu();
+  const togglePopUp = () => {
+    const popupButtons = document.querySelectorAll(".popup-btn"),
+      popup = document.querySelector(".popup"),
+      closePopUpButton = document.querySelector(".popup-close"),
+      popUpContent = document.querySelector(".popup-content");
+
+    let idInterval,
+      count = 0;
+
+    // animate popup
+    const animatePopUp = () => {
+      count++;
+      if (parseFloat(popUpContent.style.left) < 38) {
+        popUpContent.style.left = count + "%";
+      } else {
+        clearInterval(idInterval);
+        count = 0;
+      }
+    };
+
+    popupButtons.forEach((item) => {
+      item.addEventListener("click", () => {
+        popup.style.display = "block";
+        popUpContent.style.left = "-50%";
+        if (window.innerWidth > 768) {
+          idInterval = setInterval(animatePopUp, 10);
+        } else {
+          popUpContent.style.left = "38%";
+        }
+      });
+    });
+
+    closePopUpButton.addEventListener("click", () => {
+      popup.style.display = "none";
+    });
+  };
+  togglePopUp();
+
+  const scrollLinks = document.querySelectorAll("a");
+
+  for (const scrollLink of scrollLinks) {
+    scrollLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      const id = scrollLink.getAttribute("href");
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
 });
