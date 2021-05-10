349b0096bcad121c6e0dd39ac2663c40408a59c9
Elizabeth-Bakul <bakelal@mail.ru>
2021-05-10 20:09:28 +0300

    task

--- main-task/script.js
+++ main-task/script.js
@@ -506,27 +506,19 @@ window.addEventListener("DOMContentLoaded", () => {
 
       const form = document.getElementById(formIdString), // форма для заполнения заявки
         statusMessageElement = document.createElement("div"), // сообщение о статусе отправки заявки
+        popup = document.querySelector(".popup"), // pop-up окно
         inputs = document.querySelectorAll(`#${formIdString} input`); // все inputs из формы
 
       statusMessageElement.style.cssText = "font-size: 2rem; color: white";
 
       // Функция отправки данных формы на сервер
       const postData = (requestBody) => {
-        return new Promise((resolve, reject) => {
-          const request = new XMLHttpRequest();
-          request.addEventListener("readystatechange", () => {
-            if (request.readyState !== 4) {
-              return;
-            }
-            if (request.status === 200) {
-              resolve();
-            } else {
-              reject(request.statusText);
-            }
-          });
-          request.open("POST", "server.php");
-          request.setRequestHeader("Content-Type", "application/json");
-          request.send(JSON.stringify(requestBody));
+        return fetch("server.php", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(requestBody),
         });
       };
 
@@ -567,28 +559,36 @@ window.addEventListener("DOMContentLoaded", () => {
         }
       });
 
-      // Слушатель формы
+      // Слушатель формы на отправку данных
       form.addEventListener("submit", (e) => {
         e.preventDefault();
         form.append(statusMessageElement);
 
-        statusMessageElement.textContent = loadMessage;
+        statusMessageElement.textContent = loadMessage; // сообщение о загрузке
 
         const formData = new FormData(form);
         let body = {};
 
-        // берем данные формы и заполняем тело запроса body
+        // Перебор данных формы и заполнение тела запроса body
         formData.forEach((item, index) => (body[index] = item)); // у объекта formData есть свой метод forEach()
 
-        // отправка данных и уведомление пользователя
+        // Отправка данных и уведомление пользователя
         postData(body)
-          .then(() => (statusMessageElement.textContent = successMessage))
+          .then((response) => {
+            if (response.status !== 200) {
+              throw new Error("Response status code is not 200");
+            }
+            statusMessageElement.textContent = successMessage;
+          })
           .catch((error) => {
             statusMessageElement.textContent = errorMessage;
-            console.log(error);
+            console.error(error);
           });
 
         inputs.forEach((item) => (item.value = "")); // очистка input после отправки данных
+
+        setTimeout(() => statusMessageElement.remove(), 5000); // удаление сообщения о статусе
+        setTimeout(() => (popup.style.display = "none"), 7000); // закрытие модального окна
       });
     };
   sendForm("form1"); // главная форма в header
